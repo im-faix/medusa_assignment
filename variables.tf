6f023bf10a2aa444f56b05da2cffac248d322116
Mohammed Faizan <156441568+im-faix@users.noreply.github.com>
2025-04-05 17:16:40 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,8 @@
-
+variable "ecs_cluster_name" {
+  description = "The name of the ECS cluster"
+  type        = string
+  validation {
+    condition     = length(var.ecs_cluster_name) > 0
+    error_message = "ECS cluster name must not be empty."
+  }
+}
